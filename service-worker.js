109cf31b8c13f1f49c8ee12d5d162fc8b3797f46
hnoni777 <parkdam6@gmail.com>
2025-12-03 15:33:23 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,3 +5,17 @@ self.addEventListener("install", event => {
 self.addEventListener("activate", event => {
     clients.claim();
 });
+const CACHE_VERSION = 'v5'; // 숫자 올릴수록 캐시 초기화
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
